908f3bb611da77ad49fd65383b1ec89b96aad6d2
The-Olive <lpierrelouis17@mail.valenciacollege.edu>
2024-09-01 22:34:22 -0400

    Second Commit

---
+++ .venv/VariablesFun.py
@@ -0,0 +1,28 @@
+# Program to complete calculations for a student
+
+# Assigning values to each input
+firstValue= 5*6
+secondValue= 8/3
+thirdValue= 8//3
+fourthValue= 8 % 3
+fifthValue= 4200000 + 23090
+sixthValue= 4200000-7633
+
+# Input
+print("5 multiplied by 6")
+print("The answer to your query is", firstValue)
+print("")
+print("8 divided by 3")
+print("The answer to your query is {}".format(secondValue))
+print("")
+print("8 divided by 3 without the remainder")
+print("The answer to your query is", thirdValue)
+print("")
+print("8 divided by 3 (remainder only)")
+print("The answer to your query is {}".format(fourthValue))
+print("")
+print("4200000 add 23090")
+print("The answer to your query is", fifthValue)
+print("")
+print("4200000 substract 7633")
+print("The answer to your query is {}".format(sixthValue))
